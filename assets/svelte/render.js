40a0884390161cd3e9f2b14547e8b0a1fde9fb91
Wout De Puysseleir <woutdp@gmail.com>
2023-02-28 00:57:19 -0800

    Add support for the render function to render slots

--- assets/svelte/render.js
+++ assets/svelte/render.js
@@ -1,4 +1,5 @@
-module.exports.render = (name, props={}) => {
+module.exports.render = (name, props={}, slots=null) => {
     const ssrComponent = require('../../priv/static/assets/server/server.js')[name].default
-    return ssrComponent.render(props)
+    const $$slots = slots ? {default: () => slots} : {}
+    return ssrComponent.render(props, {$$slots, context: new Map()})
 }
