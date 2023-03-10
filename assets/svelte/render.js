72d4aa7e02acc0d20285409528c900f0ea1d03ce
Wout De Puysseleir <woutdp@gmail.com>
2023-03-10 13:39:34 -0800

    Refresh cache in non production env for SSR
    This way we don't need to reset the server every time

--- assets/svelte/render.js
+++ assets/svelte/render.js
@@ -1,6 +1,24 @@
-module.exports.render = (name, props={}, slots=null) => {
-    const ssrComponent = require('../../priv/static/assets/server/server.js')[name].default
-    slots = Object.fromEntries(Object.entries(slots).map(([k, v]) => [k, () => v]))
-    const $$slots = slots || {}
-    return ssrComponent.render(props, {$$slots, context: new Map()})
+const componentPath = '../../priv/static/assets/server/server.js'
+
+/***
+ * Render a component with the name, props and slots provided.
+ */
+function render(name, props = {}, slots = null) {
+    // remove from cache in non-production environments
+    // so that we can see changes
+    if (
+        process.env.NODE_ENV !== 'production' &&
+        require.resolve(componentPath) in require.cache
+    ) {
+        delete require.cache[require.resolve(componentPath)]
+    }
+
+    const component = require(componentPath)[name].default
+    const $$slots = Object.fromEntries(Object.entries(slots).map(([k, v]) => [k, () => v])) || {}
+
+    return component.render(props, { $$slots, context: new Map() })
+}
+
+module.exports = {
+    render,
 }
