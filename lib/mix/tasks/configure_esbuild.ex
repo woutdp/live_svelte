e0964be674d503c3a55380877b9c793e72f43dc7
Wout De Puysseleir <woutdp@gmail.com>
2023-03-13 15:11:42 -0700

    Only copy the files from the copy directory

--- lib/mix/tasks/configure_esbuild.ex
+++ lib/mix/tasks/configure_esbuild.ex
@@ -10,7 +10,7 @@ defmodule Mix.Tasks.LiveSvelte.ConfigureEsbuild do
 
     Mix.Project.deps_paths(depth: 1)
     |> Map.fetch!(:live_svelte)
-    |> Path.join("assets/**/*{.js}")
+    |> Path.join("assets/copy/**/*{.js}")
     |> Path.wildcard()
     |> Enum.each(fn file ->
       split = Path.split(file)
@@ -21,6 +21,7 @@ defmodule Mix.Tasks.LiveSvelte.ConfigureEsbuild do
         |> Stream.with_index()
         |> Stream.reject(fn {_item, i} -> assets_index > i end)
         |> Enum.map(&elem(&1, 0))
+        |> Enum.reject(&(&1 == "copy"))
         |> Path.join()
 
       Mix.Generator.copy_file(file, path)
--- lib/ssr.ex
+++ lib/ssr.ex
@@ -3,5 +3,5 @@ defmodule LiveSvelte.SSR do
   def render(name, nil, slots), do: render(name, %{}, slots)
 
   def render(name, props, slots),
-    do: NodeJS.call!({"svelte/render", "render"}, [name, props, slots])
+    do: NodeJS.call!({"js/render", "render"}, [name, props, slots])
 end
