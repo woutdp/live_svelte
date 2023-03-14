78c529d5248adc7d70ca6fd32294a3c07370d80f
Wout De Puysseleir <woutdp@gmail.com>
2023-03-13 18:39:13 -0700

    Set css for client components

--- assets/copy/build.js
+++ assets/copy/build.js
@@ -19,7 +19,7 @@ let optsClient = {
         importGlobPlugin(),
         sveltePlugin({
             preprocess: sveltePreprocess(),
-            compilerOptions: {hydratable: true},
+            compilerOptions: {hydratable: true, css: true},
         })
     ]
 }
