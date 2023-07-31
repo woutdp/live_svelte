dad8d2d49163f55cb86648c6c7397dc8c17b8877
Dmitrii Maganov <vonagam@gmail.com>
2023-07-31 21:16:15 +0300

    Add typescript definitions

---
+++ assets/js/live_svelte/types.d.ts
@@ -0,0 +1,3 @@
+export declare const render: (name: string, props: object, slots: object) => any
+export declare const getHooks: (components: {default: any[]; filenames: string[]}) => object
+export declare const exportSvelteComponents: (components: {default: any[]; filenames: string[]}) => object
