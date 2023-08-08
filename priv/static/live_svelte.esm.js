06632a40ab45b59a32410439624a5bfbb72d3f0d
Dmitrii Maganov <vonagam@gmail.com>
2023-08-08 21:34:36 +0300

    Refactor hooks.js

--- priv/static/live_svelte.esm.js
+++ priv/static/live_svelte.esm.js
@@ -21,172 +21,56 @@ function getRender(components) {
   };
 }
 
-// ../node_modules/svelte/internal/index.mjs
-function noop() {
+// js/live_svelte/hooks.js
+function getAttributeJson(ref, attributeName) {
+  const data = ref.el.getAttribute(attributeName);
+  return data ? JSON.parse(data) : {};
 }
-function run(fn) {
-  return fn();
-}
-function run_all(fns) {
-  fns.forEach(run);
-}
-function is_function(thing) {
-  return typeof thing === "function";
-}
-function is_empty(obj) {
-  return Object.keys(obj).length === 0;
+function detach(node) {
+  node.parentNode?.removeChild(node);
 }
 function insert(target, node, anchor) {
   target.insertBefore(node, anchor || null);
 }
-function detach(node) {
-  if (node.parentNode) {
-    node.parentNode.removeChild(node);
-  }
+function noop() {
 }
-var render_callbacks = [];
-function flush_render_callbacks(fns) {
-  const filtered = [];
-  const targets = [];
-  render_callbacks.forEach((c) => fns.indexOf(c) === -1 ? filtered.push(c) : targets.push(c));
-  targets.forEach((c) => c());
-  render_callbacks = filtered;
-}
-var globals = typeof window !== "undefined" ? window : typeof globalThis !== "undefined" ? globalThis : global;
-var _boolean_attributes = [
-  "allowfullscreen",
-  "allowpaymentrequest",
-  "async",
-  "autofocus",
-  "autoplay",
-  "checked",
-  "controls",
-  "default",
-  "defer",
-  "disabled",
-  "formnovalidate",
-  "hidden",
-  "inert",
-  "ismap",
-  "itemscope",
-  "loop",
-  "multiple",
-  "muted",
-  "nomodule",
-  "novalidate",
-  "open",
-  "playsinline",
-  "readonly",
-  "required",
-  "reversed",
-  "selected"
-];
-var boolean_attributes = /* @__PURE__ */ new Set([..._boolean_attributes]);
-function destroy_component(component, detaching) {
-  const $$ = component.$$;
-  if ($$.fragment !== null) {
-    flush_render_callbacks($$.after_update);
-    run_all($$.on_destroy);
-    $$.fragment && $$.fragment.d(detaching);
-    $$.on_destroy = $$.fragment = null;
-    $$.ctx = [];
-  }
-}
-var SvelteElement;
-if (typeof HTMLElement === "function") {
-  SvelteElement = class extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({ mode: "open" });
-    }
-    connectedCallback() {
-      const { on_mount } = this.$$;
-      this.$$.on_disconnect = on_mount.map(run).filter(is_function);
-      for (const key in this.$$.slotted) {
-        this.appendChild(this.$$.slotted[key]);
-      }
-    }
-    attributeChangedCallback(attr, _oldValue, newValue) {
-      this[attr] = newValue;
-    }
-    disconnectedCallback() {
-      run_all(this.$$.on_disconnect);
-    }
-    $destroy() {
-      destroy_component(this, 1);
-      this.$destroy = noop;
-    }
-    $on(type, callback) {
-      if (!is_function(callback)) {
-        return noop;
-      }
-      const callbacks = this.$$.callbacks[type] || (this.$$.callbacks[type] = []);
-      callbacks.push(callback);
-      return () => {
-        const index = callbacks.indexOf(callback);
-        if (index !== -1)
-          callbacks.splice(index, 1);
-      };
-    }
-    $set($$props) {
-      if (this.$$set && !is_empty($$props)) {
-        this.$$.skip_bound = true;
-        this.$$set($$props);
-        this.$$.skip_bound = false;
-      }
-    }
-  };
-}
-
-// js/live_svelte/hooks.js
-function base64ToElement(base64) {
-  const template = document.createElement("div");
-  template.innerHTML = atob(base64).trim();
-  return template;
-}
-function dataAttributeToJson(attributeName, el) {
-  const data = el.getAttribute(attributeName);
-  return data ? JSON.parse(data) : {};
-}
-function createSlots(slots, ref) {
-  const createSlot = (slotName, ref2) => {
-    let savedTarget, savedAnchor, savedElement;
-    return () => {
+function getSlots(ref) {
+  const slots = {};
+  for (const slotName in getAttributeJson(ref, "data-slots")) {
+    const slot = () => {
       return {
         getElement() {
-          return base64ToElement(dataAttributeToJson("data-slots", ref2.el)[slotName]);
+          const base64 = getAttributeJson(ref, "data-slots")[slotName];
+          const element = document.createElement("div");
+          element.innerHTML = atob(base64).trim();
+          return element;
         },
         update() {
-          const element = this.getElement();
-          detach(savedElement);
-          insert(savedTarget, element, savedAnchor);
-          savedElement = element;
+          detach(this.savedElement);
+          this.savedElement = this.getElement();
+          insert(this.savedTarget, this.savedElement, this.savedAnchor);
         },
         c: noop,
         m(target, anchor) {
-          const element = this.getElement();
-          savedTarget = target;
-          savedAnchor = anchor;
-          savedElement = element;
-          insert(target, element, anchor);
+          this.savedTarget = target;
+          this.savedAnchor = anchor;
+          this.savedElement = this.getElement();
+          insert(this.savedTarget, this.savedElement, this.savedAnchor);
         },
         d(detaching) {
           if (detaching)
-            detach(savedElement);
+            detach(this.savedElement);
         },
         l: noop
       };
     };
-  };
-  const svelteSlots = {};
-  for (const slotName in slots) {
-    svelteSlots[slotName] = [createSlot(slotName, ref)];
+    slots[slotName] = [slot];
   }
-  return svelteSlots;
+  return slots;
 }
 function getLiveJsonProps(ref) {
-  const json = dataAttributeToJson("data-live-json", ref.el);
-  if (typeof json === "object" && json !== null && !Array.isArray(json))
+  const json = getAttributeJson(ref, "data-live-json");
+  if (!Array.isArray(json))
     return json;
   const liveJsonData = {};
   for (const liveJsonVariable of json) {
@@ -198,10 +82,10 @@ function getLiveJsonProps(ref) {
 }
 function getProps(ref) {
   return {
-    ...dataAttributeToJson("data-props", ref.el),
+    ...getAttributeJson(ref, "data-props"),
     ...getLiveJsonProps(ref),
     live: ref,
-    $$slots: createSlots(dataAttributeToJson("data-slots", ref.el), ref),
+    $$slots: getSlots(ref),
     $$scope: {}
   };
 }
@@ -220,7 +104,7 @@ function getHooks(components) {
       if (!Component) {
         throw new Error(`Unable to find ${componentName} component.`);
       }
-      for (const liveJsonElement of Object.keys(dataAttributeToJson("data-live-json", this.el))) {
+      for (const liveJsonElement of Object.keys(getAttributeJson(this, "data-live-json"))) {
         window.addEventListener(`${liveJsonElement}_initialized`, (event) => this._instance.$set(getProps(this)), false);
         window.addEventListener(`${liveJsonElement}_patched`, (event) => this._instance.$set(getProps(this)), false);
       }
