d513b8b78e246d915fee7b6b69315e728116e0bc
Dmitrii Maganov <vonagam@gmail.com>
2023-08-18 07:56:50 +0300

    Fix optional arguments in typescript definitions

--- assets/js/live_svelte/types.d.ts
+++ assets/js/live_svelte/types.d.ts
@@ -1,6 +1,6 @@
 export type Live = {
-    pushEvent(event: string, payload: object, onReply: (reply: any, ref: number) => void): number
-    pushEventTo(phxTarget: any, event: string, payload: object, onReply: (reply: any, ref: number) => void): number
+    pushEvent(event: string, payload?: object, onReply?: (reply: any, ref: number) => void): number
+    pushEventTo(phxTarget: any, event: string, payload?: object, onReply?: (reply: any, ref: number) => void): number
 
     handleEvent(event: string, callback: (payload: any) => void): Function
     removeHandleEvent(callbackRef: Function): void
