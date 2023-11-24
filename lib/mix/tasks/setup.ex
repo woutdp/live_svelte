d29913efb783401cbdd0af3cd9553a56962c69a3
Tony Dang <tony@tonydang.blog>
2023-11-23 16:39:21 -0800

    run setup tasks synchronously

--- lib/mix/tasks/setup.ex
+++ lib/mix/tasks/setup.ex
@@ -11,8 +11,10 @@ defmodule Mix.Tasks.LiveSvelte.Setup do
       "configure_phoenix",
       "configure_esbuild"
     ]
-    |> Enum.map(&Task.async(fn -> Mix.Task.run("live_svelte." <> &1) end))
-    |> Enum.map(&Task.await(&1, :infinity))
+    |> Enum.map(
+      &(Task.async(fn -> Mix.Task.run("live_svelte." <> &1) end)
+        |> Task.await(:infinity))
+    )
 
     log_success("live_svelte setup finished.")
   end
