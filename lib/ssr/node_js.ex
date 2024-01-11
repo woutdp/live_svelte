e51cc188bb1d7f8fc33d4ecbd1d3a3b7ca22882a
drKreso <kresimir.bojcic@gmail.com>
2024-01-11 11:16:05 +0100

    properly encode server components

--- lib/ssr/node_js.ex
+++ lib/ssr/node_js.ex
@@ -4,13 +4,14 @@ defmodule LiveSvelte.SSR.NodeJS do
 
   def render(name, props, slots) do
     try do
-      NodeJS.call!({"server", "render"}, [name, props, slots])
+      NodeJS.call!({"server", "render"}, [name, props, slots], binary: true)
     catch
       :exit, {:noproc, _} ->
         message = """
         NodeJS is not configured. Please add the following to your application.ex:
         {NodeJS.Supervisor, [path: LiveSvelte.SSR.NodeJS.server_path(), pool_size: 4]},
         """
+
         raise %LiveSvelte.SSR.NotConfigured{message: message}
     end
   end
