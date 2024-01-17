055632f2bd7aa828cb9dbd7e00be49d6938ef181
Wout De Puysseleir <woutdp@gmail.com>
2024-01-17 12:21:45 -0800

    Upgrade to 1.16 for example_project

--- example_project/mix.exs
+++ example_project/mix.exs
@@ -5,7 +5,7 @@ defmodule Example.MixProject do
     [
       app: :example,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
