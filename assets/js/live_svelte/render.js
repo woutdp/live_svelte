aab14cbd6d46f93708c19a4c036d4c533ca2cb3c
Wout De Puysseleir <woutdp@gmail.com>
2024-10-19 12:14:54 -0700

    Use render function Svelte 5

--- assets/js/live_svelte/render.js
+++ assets/js/live_svelte/render.js
@@ -1,11 +1,12 @@
+import {render} from "svelte/server"
 import {normalizeComponents} from "./utils"
 
 export function getRender(components) {
     components = normalizeComponents(components)
 
-    return function render(name, props, slots) {
+    return (name, props, slots) => {
         const Component = components[name]
         const $$slots = Object.fromEntries(Object.entries(slots).map(([k, v]) => [k, () => v]))
-        return Component.render(props, {$$slots})
+        return render(Component, props, {props: {$$slots}})
     }
 }
