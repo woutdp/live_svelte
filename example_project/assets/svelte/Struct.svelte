2710cd7157c27c475130d2946f291102c232b15a
Wout De Puysseleir <woutdp@gmail.com>
2024-11-11 11:37:17 -0800

    Add randomization to Struct example
    This is a nice way to test if maps are updating properly

--- example_project/assets/svelte/Struct.svelte
+++ example_project/assets/svelte/Struct.svelte
@@ -1,6 +1,8 @@
 <script>
-    /** @type {{struct: any}} */
     let {struct} = $props()
 </script>
 
-{JSON.stringify(struct)}
+<div class="flex flex-col gap-2">
+    {JSON.stringify(struct)}
+    <button phx-click="randomize" class="bg-black text-white rounded p-2">Randomize Age</button>
+</div>
