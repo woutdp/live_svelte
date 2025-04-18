03323da26d11f7247ff6a40af3ca19ca1e3b509f
Wout De Puysseleir <woutdp@gmail.com>
2025-04-18 09:06:44 -0700

    Update flake file

--- flake.nix
+++ flake.nix
@@ -1,21 +1,31 @@
 {
-  description =
-    "A flake for building development environment of Phoenix project.";
+  description = "A flake for building development environment of Phoenix project.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in with pkgs; {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs;
+      {
         devShells.default = mkShell {
           buildInputs =
             [
-              erlang_26 beam.packages.erlang_26.elixir_1_17
+              erlang_27
+              beam.packages.erlang_26.elixir_1_18
               nodejs_22
+              gh
             ]
             ++ lib.optionals stdenv.isLinux [
               # For ExUnit Notifier on Linux.
@@ -23,7 +33,8 @@
 
               # For file_system on Linux.
               inotify-tools
-            ] ++ lib.optionals stdenv.isDarwin ([
+            ]
+            ++ lib.optionals stdenv.isDarwin ([
               # For ExUnit Notifier on macOS.
               terminal-notifier
 
@@ -49,6 +60,6 @@
             export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$PWD/.erlang-history\"'"
           '';
         };
-      });
+      }
+    );
 }
-
