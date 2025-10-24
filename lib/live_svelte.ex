e07aede2e4aa7b7d225096997fd36c532d14b99a
Christian Wesselhoeft <hi@xtian.us>
2025-10-24 10:10:05 -0600

    svelte/1: Add csp_nonce parameter

--- lib/live_svelte.ex
+++ lib/live_svelte.ex
@@ -28,6 +28,10 @@ defmodule LiveSvelte do
     doc: "Class to apply to the Svelte component",
     examples: ["my-class", "my-class another-class"]
 
+  attr :csp_nonce, :string,
+    default: nil,
+    doc: "A Content-Security-Policy nonce for the <script> and <style> tags"
+
   attr :ssr, :boolean,
     default: true,
     doc: "Whether to render the component via NodeJS on the server",
@@ -84,14 +88,16 @@ defmodule LiveSvelte do
       end
 
     assigns =
-      assigns
-      |> assign(:init, init)
-      |> assign(:slots, slots)
-      |> assign(:ssr_render, ssr_code)
+      assign(assigns,
+        csp_attrs: if(nonce = assigns[:csp_nonce], do: [nonce: nonce]),
+        init: init,
+        slots: slots,
+        ssr_render: ssr_code
+      )
 
     ~H"""
     <.live_json live_json_props={@live_json_props}>
-      <script>
+      <script {@csp_attrs}>
         <%= raw(@ssr_render["head"]) %>
       </script>
       <div
@@ -108,7 +114,7 @@ defmodule LiveSvelte do
         class={@class}
       >
         <%= raw(@ssr_render["head"]) %>
-        <style>
+        <style {@csp_attrs}>
           <%= raw(@ssr_render["css"]["code"]) %>
         </style>
         <%= raw(@ssr_render["html"]) %>
